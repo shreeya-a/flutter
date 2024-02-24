408f28114d208474070d35c8d2b7e224cdd41fc8
shreeya-a <shreeyadhikari11@gmail.com>
2024-02-24 13:06:57 +0545

    Day 1

---
+++ D1/function.dart
@@ -0,0 +1,13 @@
+void main() {
+int result = mul(5, 4);
+int resultd = div(result);
+  print (resultd);
+}
+int mul(int a, int b){
+  var result = a * b;
+  return result;
+}
+int div(result){
+  var r = result * 2;
+  return r;
+}
